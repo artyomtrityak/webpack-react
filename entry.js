46ad9e0dd560b9205aa088596662250a06380491
Artyom Trityak <art.trityak@gmail.com>
2015-03-18 22:01:33 +0200

    babel

--- entry.js
+++ entry.js
@@ -1,3 +1,11 @@
+var React = require('react');
+
+var res = require('./content.js');
+
+require("./style.css");
+
+var rr = require('./re');
+
 class SkinnedMesh {
   constructor(geometry, materials) {
     super(geometry, materials);
